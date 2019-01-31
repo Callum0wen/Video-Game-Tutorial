8385c4b0329584c6f1a89b13bbbce47ac5a37127
Callum Owen <34078115+Callum0wen@users.noreply.github.com>
2019-01-31 21:35:22 +0000

    Completed up to tutorial 9
    Added player movement, obstacles with collision detection, game fail conditions, and a game win with animation that leads to the next scene/level.

---
+++ VideoGameTutorial/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Complete Level");
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame ()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restard", restartDelay);
+        }
+    }
+
+    void Restard()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ VideoGameTutorial/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    //"Fixed" update because it is for updating the physics of the object.
+    void FixedUpdate()
+    {
+        //Add a player force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);   //Add a force of 2000 to the object
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
